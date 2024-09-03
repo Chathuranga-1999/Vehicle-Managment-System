51b79e47b3eab9014f041a46f3b24e24f77628bf
Janidu-2000 <wathsarajanidu@gmail.com>
2024-09-04 00:14:48 +0530

    Changes

--- service -record-service/service-record-service/src/main/java/com/vehicle_management_system/service/_record_service/repository/servicerecordrepository.java
+++
@@ -1,7 +0,0 @@
-package com.vehicle_management_system.service._record_service.repository;
-
-import com.vehicle_management_system.service._record_service.model.ServiceRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface servicerecordrepository extends JpaRepository<ServiceRecord,Long> {
-}
--- service -record-service/service-record-service/src/main/java/com/vehicle_management_system/service/_record_service/service/ApiService.java
+++
@@ -1,4 +0,0 @@
-package com.vehicle_management_system.service._record_service.service;
-
-public interface ApiService {
-}
