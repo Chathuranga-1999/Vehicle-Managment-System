f42f2ae28d39e135f6423d66a8c3be5963d5b993
Chathuranga-1999 <chathu.redfox@gmail.com>
2024-09-06 01:58:48 +0530

    add login-service

---
+++ login-service/src/test/java/com/Vehicle_Managment_System/login_service/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vehicle_Managment_System.login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
